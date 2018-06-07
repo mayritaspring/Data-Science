5edfd3a2df04af08d0a726088256236aa08982cb
https://github.com/mayritaspring <mayritaspring@gmail.com>
2018-06-07 21:49:40 +0800

    adjist XGBoost code

--- XGBoost/README.md
+++ XGBoost/README.md
@@ -1,32 +1,34 @@
 XGBoost has a large number of advanced parameters, which can all affect the quality and speed of your model.
 
-- max_depth : int
+- max_depth : int,
     Maximum tree depth for base learners.
-- learning_rate : float
+- learning_rate : float,
     Boosting learning rate (XGBoost's "eta")
-- n_estimators : int
+- n_estimators : int,
     Number of boosted trees to fit.
-- silent : boolean
+- silent : boolean,
     Whether to print messages while running boosting.
-- objective : string
+- objective : string,
     Specify the learning task and the corresponding learning objective.
-- nthread : int
+- nthread : int,
     Number of parallel threads used to run XGBoost.
-- gamma : float
+- gamma : float,
     Minimum loss reduction required to make a further partition
     on a leaf node of the tree.
-- min_child_weight : int
+- min_child_weight : int,
     Minimum sum of instance weight(hessian) needed in a child.
-- max_delta_step : int
+- max_delta_step : int,
     Maximum delta step we allow each tree's weight estimation to be.
-- subsample : float
+- subsample : float,
     Subsample ratio of the training instance.
-- colsample_bytree : float
+- colsample_bytree : float,
     Subsample ratio of columns when constructing each tree.
 - base_score:
     The initial prediction score of all instances, global bias.
-- seed : int
+- seed : int,
     Random number seed.
-- missing : float, optional
+- missing : float, optional,
     Value in the data which needs to be present as a missing value.
     If None, defaults to np.nan.
+	
+XGBoost can take a sparse matrix as input. This allows you to convert categorical variables with high cardinality into a dummy matrix, then build a model without getting an out of memory error!
--- XGBoost/XGBoost.py
+++ XGBoost/XGBoost.py
@@ -1,6 +1,7 @@
 #install package
 from numpy import loadtxt
 from xgboost import XGBClassifier
+import xgboost as xgb
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
@@ -25,6 +26,8 @@ credit_data.columns
 credit_data.index
 credit_data.info()
 credit_data.describe()
+credit_data.dtypes()
+credit_data['LIMIT_BAL'] = credit_data['LIMIT_BAL'].astype('category')
 credit_data.groupby(['default payment next month']).count() #check number of y=1 
 
 #Split to Training and Testing
@@ -50,7 +53,76 @@ print("Accuracy: %.2f%%" % (accuracy * 100.0))
 
 
 
+#tune parameter
+#use crtl+1 to select all
+#from pandas.core.categorical import Categorical
+#from scipy.sparse import csr_matrix
+#import numpy as np
+#
+#def sparse_dummies(categorical_values):
+#    categories = Categorical.from_array(categorical_values)
+#    N = len(categorical_values)
+#    row_numbers = np.arange(N, dtype=np.int)
+#    ones = np.ones((N,))
+#    return csr_matrix( (ones, (row_numbers, categories.codes)) )
+#
+#sparse_dummies(X.LIMIT_BAL)
+#8from scipy.sparse import hstack
+#cat1 = sparse_dummies(df.VAR_0001)
+#cat2 = sparse_dummies(df.VAR_0002)
+#hstack((cat1,cat2), format="csr")
 
+
+clf = xgb.XGBClassifier(n_estimators=10000)
+eval_set  = [(X_train,y_train), (X_test,y_test)]
+clf.fit(X_train, y_train, eval_set=eval_set, eval_metric="auc", early_stopping_rounds=30)
+
+#You can get the features importance easily in clf.booster().get_fscore() where clf is your trained classifier.
+features = [ "your list of features ..." ]
+mapFeat = dict(zip(["f"+str(i) for i in range(len(features))],features))
+ts = pd.Series(clf.booster().get_fscore())
+ts.index = ts.reset_index()['index'].map(mapFeat)
+ts.order()[-15:].plot(kind="barh", title=("features importance"))
+
+
+
+########debug###############
+#Using Hyperopt For Grid Searching
+from sklearn.metrics import roc_auc_score
+from hyperopt import hp, fmin, tpe, STATUS_OK, Trials
+
+def objective(space):
+    clf = xgb.XGBClassifier(n_estimators = 10000,
+                            max_depth = space['max_depth']#,
+                            #min_child_weight = space['min_child_weight'],
+                            #subsample = space['subsample']
+                            )
+
+    eval_set  = [(X_train, y_train), (X_test, y_test)]
+    clf.fit(X_train, y_train, eval_set=eval_set, eval_metric = "auc", early_stopping_rounds=30)
+
+    pred = clf.predict_proba(X_test)[:,1]
+    auc = roc_auc_score(y_test, pred)
+    print("SCORE:", auc)
+    return{'loss':1-auc, 'status': STATUS_OK }
+
+
+space ={
+        'max_depth': hp.choice('x_max_depth', np.arange(5, 30, dtype=int))#,
+        #'max_depth': hp.quniform("x_max_depth", 5, 30, 1),
+        #'min_child_weight': hp.quniform ('x_min_child', 1, 10, 1),
+        #'subsample': hp.uniform ('x_subsample', 0.8, 1)
+    }
+
+
+trials = Trials()
+best = fmin(fn=objective,
+            space=space,
+            algo=tpe.suggest,
+            max_evals=100,
+            trials=trials)
+
+print(best)
 
 
 # Random Forest
