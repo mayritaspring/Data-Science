529b158cc4a8dced54e37e8b6191e0673284f00e
周宛誼 <mayritaspring@zhouwanyide-MacBook-Pro.local>
2018-08-25 16:42:55 +0800

    add FM_try different packages.py

--- Other/VisionAPI.py
+++ Other/VisionAPI.py
@@ -33,11 +33,14 @@ def explicit():
     # Explicitly use service account credentials by specifying the private key
     # file.
     storage_client = storage.Client.from_service_account_json(
-        'service_account.json')
+        'Picture_Recognition-2d7797dd31f3.json')
 
     # Make an authenticated API request
     buckets = list(storage_client.list_buckets())
     print(buckets)    
+   
+    
+    
     
 # Instantiates a client
 client = vision.ImageAnnotatorClient()
